99264ef5b506b0499d6f933a1364232a957e0b86
Andrei Egorov <andreyegorov1992@gmail.com>
2019-05-07 23:37:28 -0700

    Add Admin, posts, about routes

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about-page">
+        <h1> This is my About me page</h1>
+      <p>About me page! About me page! About me page! About me page! About me page!</p>
+      <p>About me page! About me page! About me page! About me page! About me page!</p>
+      <p>About me page! About me page! About me page! About me page! About me page!</p>
+      <p>About me page! About me page! About me page! About me page! About me page!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+  .about-page{
+    text-align: center;
+    padding: 30px
+  }
+</style>
