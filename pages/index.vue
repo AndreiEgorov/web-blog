8f8a7456c920d20c7931bfa24fc7aed60ef61d26
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-05-07 23:18:56 -0700

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        blog-website
-      </h1>
-      <h2 class="subtitle">
-        Web blog built with Nuxt, vuejs, vuex, node, axios
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <h1>Main page</h1>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
