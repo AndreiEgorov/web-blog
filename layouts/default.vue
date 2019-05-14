4f2a1f0ab01feca27eb0822563c82097551c05f5
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-05-14 07:57:42 -0700

    Hamburger Menu, Sidebar, Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidnav = !displaySidnav"/>
+    <TheSidenav
+      :show="displaySidnav"
+      @close="displaySidnav= false"
+    />
     <nuxt />
   </div>
 </template>
-
+<script>
+  import TheHeader from '~/components/Navigation/TheHeader'
+  import TheSidenav from '~/components/Navigation/TheSidenav'
+  export default {
+    components:{
+      TheHeader,
+      TheSidenav
+    },
+    data(){
+      return{
+        displaySidnav: false
+      }
+    }
+  }
+</script>
 <style>
   html{
     font-family: 'Roboto Mono', sans-serif;
