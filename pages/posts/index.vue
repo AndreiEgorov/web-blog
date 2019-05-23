1e60a05aa94a3f5435ca4721fac727fc3772ec37
Andrei Egorov <andreyegorov1992@gmail.com>
2019-05-23 00:07:17 -0700

    add nuxtServerInit METHOD to preload important data only once on server side

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,46 +13,6 @@
       PostList
     },
 
-    fetch(context) {
-      to only fetch if (aka prepopulate store state) if there is nothing in it
-      if(context.store.state.loadedPosts.length > 0) {
-        return null
-      }
-      return new Promise((resolve, reject) => {
-        // in case of successful resolve the below happens
-
-        setTimeout(() => {
-
-          resolve({
-            loadedPosts: [
-              {
-                id: "1",
-                title: "HOOHOH",
-                previewText: "THIS IS  our post",
-                thumbnail: "https://zdnet1.cbsistatic.com/hub/i/2018/01/26/b4fe5bfc-6e3b-4575-b8db-f06caadc1a71/b54b41fb82647ceee2c18a6912f0e8db/tech-transport-future-intro.jpg"
-              },
-              {
-                id: "2",
-                title: "SECOND POSt",
-                previewText: "THIS IS  our post",
-                thumbnail: "https://zdnet1.cbsistatic.com/hub/i/2018/01/26/b4fe5bfc-6e3b-4575-b8db-f06caadc1a71/b54b41fb82647ceee2c18a6912f0e8db/tech-transport-future-intro.jpg"
-
-              }
-            ]
-          })
-        }, 2000)
-        // in case of reject the below happens and nuxt error page comes up
-        // reject(new Error())
-      }).then(data => {
-          // access store via context since inside fetch you dont have access to "this"
-        context.store.commit('setPosts', data.loadedPosts)
-        }
-
-      ).catch(e => {
-        //context.error will bring up the nuxt error page up
-        context.error(e)
-      });
-    },
    computed:{
       loadedPosts() {
         return this.$store.getters.loadedPosts
--- store/index.js
+++ store/index.js
@@ -7,18 +7,40 @@ const createStore = () => {
     state: {
       loadedPosts: []
     },
-    mutations:{
-      setPosts(state, posts){
+    mutations: {
+      setPosts(state, posts) {
         state.loadedPosts = posts
       }
     },
-    actions:{
-      setPosts(vuexContext, posts){
+    actions: {
+      // nuxtServerInit action will be dispatched automatically by nuxt only once to preload data into store
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+
+          setTimeout(() => {
+            vuexContext.commit('setPosts', [ {
+              id: "1",
+              title: "HOOHOH",
+              previewText: "THIS IS  our post",
+              thumbnail: "https://zdnet1.cbsistatic.com/hub/i/2018/01/26/b4fe5bfc-6e3b-4575-b8db-f06caadc1a71/b54b41fb82647ceee2c18a6912f0e8db/tech-transport-future-intro.jpg"
+            },
+              {
+                id: "2",
+                title: "SECOND POSt",
+                previewText: "THIS IS  our post",
+                thumbnail: "https://zdnet1.cbsistatic.com/hub/i/2018/01/26/b4fe5bfc-6e3b-4575-b8db-f06caadc1a71/b54b41fb82647ceee2c18a6912f0e8db/tech-transport-future-intro.jpg"
+
+              }])
+            resolve()
+          }, 2000)
+        })
+      },
+      setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
       }
     },
-    getters:{
-      loadedPosts(state){
+    getters: {
+      loadedPosts(state) {
         return state.loadedPosts
       }
     }
@@ -26,3 +48,6 @@ const createStore = () => {
 }
 
 export default createStore
+
+
+
